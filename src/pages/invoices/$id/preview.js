783e027ec6c730d72cd71fc43c700d6238bc498b
Madis Väin <madisvain@gmail.com>
2019-04-19 19:32:03 +0300

    add bootstrap styling to invoices

--- src/pages/invoices/$id/preview.js
+++ src/pages/invoices/$id/preview.js
@@ -17,53 +17,69 @@ class InvoicePreview extends Component {
     return (
       <div>
         {client && invoice ? (
-          <Frame style={{ border: 'none', width: '100%', height: '100vh' }}>
-            <h1>Invoice nr. {get(invoice, 'number')}</h1>
-            <p style={{ whiteSpace: 'pre-line' }}>
-              {get(client, 'name')}
-              {has(client, 'address') ? (
-                <span>
-                  <br />
-                  {get(client, 'address')}
-                </span>
-              ) : null}
-              {has(client, 'emails') ? (
-                <span>
-                  <br />
-                  <a href={`mailto:${head(get(client, 'emails', []))}`}>
-                    {head(get(client, 'emails', []))}
-                  </a>
-                </span>
-              ) : null}
-              {has(client, 'website') ? (
-                <span>
-                  <br />
-                  <a href={get(client, 'website')}>{get(client, 'website')}</a>
-                </span>
-              ) : null}
-            </p>
-            <table style={{ width: '100%' }}>
-              <thead>
-                <tr>
-                  <td>#</td>
-                  <td>Description</td>
-                  <td>Quantity</td>
-                  <td>Price</td>
-                  <td>Sum</td>
-                </tr>
-              </thead>
-              <tbody>
-                {get(invoice, 'lineItems', []).map((lineItem, index) => (
-                  <tr>
-                    <td>{index + 1}</td>
-                    <td>{lineItem.description}</td>
-                    <td>{lineItem.quantity}</td>
-                    <td>{lineItem.unitPrice}</td>
-                    <td>{lineItem.subtotal}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          <Frame
+            style={{ border: 'none', width: '100%', height: '100vh' }}
+            head={
+              <link
+                rel="stylesheet"
+                href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                crossorigin="anonymous"
+              />
+            }
+          >
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col">
+                  <h1>Invoice #{get(invoice, 'number')}</h1>
+                  <div style={{ whiteSpace: 'pre-line', marginBottom: 40 }}>
+                    {get(client, 'name')}
+                    {has(client, 'address') ? (
+                      <span>
+                        <br />
+                        {get(client, 'address')}
+                      </span>
+                    ) : null}
+                    {has(client, 'emails') ? (
+                      <span>
+                        <br />
+                        <a href={`mailto:${head(get(client, 'emails', []))}`}>
+                          {head(get(client, 'emails', []))}
+                        </a>
+                      </span>
+                    ) : null}
+                    {has(client, 'website') ? (
+                      <span>
+                        <br />
+                        <a href={get(client, 'website')}>{get(client, 'website')}</a>
+                      </span>
+                    ) : null}
+                  </div>
+                  <table className="table">
+                    <thead>
+                      <tr>
+                        <td>#</td>
+                        <td>Description</td>
+                        <td>Quantity</td>
+                        <td>Price</td>
+                        <td>Sum</td>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {get(invoice, 'lineItems', []).map((lineItem, index) => (
+                        <tr>
+                          <td>{index + 1}</td>
+                          <td>{lineItem.description}</td>
+                          <td>{lineItem.quantity}</td>
+                          <td>{lineItem.unitPrice}</td>
+                          <td>{lineItem.subtotal}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </Frame>
         ) : (
           <div style={{ textAlign: 'center', paddingTop: 100 }}>
