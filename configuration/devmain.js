70ac5e91e427392ae044847324bdfeb817e3740f
Madis Väin <madisvain@gmail.com>
2019-11-17 13:42:55 +0200

    use npm not yarn

--- configuration/devmain.js
+++ configuration/devmain.js
@@ -1,6 +1,6 @@
-const path =  require('path');
+const path = require('path');
 const { execSync } = require('child_process');
 
 setTimeout(() => {
-    execSync('yarn dev:main', { cwd: path.join(__dirname, '..') })
+  execSync('npm run dev:main', { cwd: path.join(__dirname, '..') });
 }, 8000);
