13f9e36bf46d90996fa46f275ff50a5fce4e2feb
Madis Väin <madisvain@gmail.com>
2019-12-23 15:51:00 +0200

    update packages and improve website

--- website/pages/index.js
+++ website/pages/index.js
@@ -6,79 +6,93 @@ export default () => (
       <span className="navbar-brand h1">
         <img src="/logo.svg" className="img-fluid" alt="Logo" />
       </span>
-      <ul class="navbar-nav ml-auto">
+      <ul className="navbar-nav ml-auto">
         <a href="https://github.com/madisvain/upcount" target="_blank">
           <img src="/github.svg" className="img-fluid" alt="Logo" />
         </a>
       </ul>
     </nav>
-    <div className="row" style={{ marginTop: 40 }}>
-      <div className="col-8 offset-2">
-        <h1 className="text-center">Easy Invocing</h1>
-        <ul className="list-inline text-center mt-4">
-          <li class="list-inline-item">
-            <button type="button" className="btn btn-primary">
-              Mac
-            </button>
-          </li>
-          <li class="list-inline-item">
-            <button type="button" className="btn btn-outline-secondary">
-              Windows
-            </button>
-          </li>
-          <li class="list-inline-item">
-            <button type="button" className="btn btn-outline-secondary">
-              Linux
-            </button>
-          </li>
-        </ul>
-        <p className="text-center">
-          <small>
-            Upcount is free to download and use and is available for Windows, Mac and Linux.
-          </small>
-        </p>
-        <figure class="figure">
-          <img src="/illustration.svg" className="figure-img img-fluid" alt="Illustration" />
-          <figcaption class="figure-caption text-right">
-            <blockquote class="blockquote">
-              <p className="mb-0">“Cats will outsmart dogs every time.”</p>
-              <footer class="blockquote-footer">John Grogan</footer>
-            </blockquote>
-          </figcaption>
-        </figure>
+
+    {/* Landing & Download */}
+    <div className="container">
+      <div className="row" style={{ marginTop: 40 }}>
+        <div className="col-8 offset-2">
+          <h1 className="text-center">Easy Invocing</h1>
+          <ul className="list-inline text-center mt-4">
+            <li className="list-inline-item">
+              <button type="button" className="btn btn-primary">
+                Mac
+              </button>
+            </li>
+            <li className="list-inline-item">
+              <button type="button" className="btn btn-outline-secondary">
+                Windows
+              </button>
+            </li>
+            <li className="list-inline-item">
+              <button type="button" className="btn btn-outline-secondary">
+                Linux
+              </button>
+            </li>
+          </ul>
+          <p className="text-center">
+            <small>
+              Upcount is free to download and use and is available for Windows, Mac and Linux.
+            </small>
+          </p>
+          <figure className="figure">
+            <img src="/illustration.svg" className="figure-img img-fluid" alt="Illustration" />
+            <figcaption className="figure-caption text-right">
+              <blockquote className="blockquote">
+                <p className="mb-0">“Cats will outsmart dogs every time.”</p>
+                <footer className="blockquote-footer">John Grogan</footer>
+              </blockquote>
+            </figcaption>
+          </figure>
+        </div>
       </div>
     </div>
-    <div className="container-fluid">
+
+    {/* Screenshots */}
+    <div className="container">
       <div className="row" style={{ marginTop: 80 }}>
         <div className="col col-sm-6">
           <img src="screenshots/invoices.png" className="img-fluid" alt="Invoices list" />
         </div>
         <div className="col col-sm-6">
-          <h2 className="mt-4 mb-4">Upcount invoicing app</h2>
+          <h2 className="mt-4 mb-4">Keep invoices in check</h2>
           <p>
             Upcount is a beautifully-simple desktop app designed to help manage your invoicing.
             Easily keep track of invoice payments, overdue invoices via invoice states.
-            <ul class="list-unstyled mt-2">
-              <li>
-                <span class="badge badge-secondary">Draft</span>
-              </li>
-              <li>
-                <span class="badge badge-primary">Confirmed</span>
-              </li>
-              <li>
-                <span class="badge badge-success">Payed</span>
-              </li>
-              <li>
-                <span class="badge badge-warning">Overdue</span>
-              </li>
-              <li>
-                <span class="badge badge-danger">Void</span>
-              </li>
-            </ul>
           </p>
+          <ul className="list-unstyled mt-2">
+            <li>
+              <span className="badge badge-secondary">Draft</span>
+            </li>
+            <li>
+              <span className="badge badge-primary">Confirmed</span>
+            </li>
+            <li>
+              <span className="badge badge-success">Payed</span>
+            </li>
+            <li>
+              <span className="badge badge-warning">Overdue</span>
+            </li>
+            <li>
+              <span className="badge badge-danger">Void</span>
+            </li>
+          </ul>
         </div>
       </div>
       <div className="row" style={{ marginTop: 80 }}>
+        <div className="col col-sm-6">
+          <h2 className="mt-4 mb-4 text-right">Clean user experience</h2>
+          <p className="text-right">
+            Easy to understand and use invoice creation interface.
+            <br />
+            <i>What you see is what you get.</i>
+          </p>
+        </div>
         <div className="col col-sm-6">
           <img src="screenshots/invoice-edit.png" className="img-fluid" alt="Invoice editing" />
         </div>
@@ -86,6 +100,27 @@ export default () => (
       <div className="row" style={{ marginTop: 80 }}>
         <div className="col col-sm-6">
           <img src="screenshots/settings.png" className="img-fluid" alt="Invoice settings" />
+        </div>
+        <div className="col col-sm-6">
+          <h2 className="mt-4 mb-4">Configurable options</h2>
+          <p>Customize the invoices according to your needs and preferences.</p>
+          <ul className="list-unstyled mt-2">
+            <li>Upload your company logo</li>
+            <li>Set payment terms & overdue charges</li>
+            <li>Set up tax rates</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    {/* Footer */}
+    <div className="container">
+      <div className="row" style={{ marginTop: 100, marginBottom: 20 }}>
+        <div className="col text-center">
+          Made with <span style={{ color: '#e25555' }}>&hearts;</span> for coding by{' '}
+          <a href="https://github.com/madisvain">Madis Väin</a>
+          <br />
+          <small>Estonia, Tallinn</small>
         </div>
       </div>
     </div>
